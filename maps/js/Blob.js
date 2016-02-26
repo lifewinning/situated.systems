28d63b8e9b65cd4c6b16241c75067f4b1aab3d9a
lifewinning <lifewinning@gmail.com>
2016-02-26 10:05:19 -0800

    me showing erryone git

---
+++ maps/js/Blob.js
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US-x-Hixie">
+<head>
+<meta charset="utf-8" />
+<title>Page not found &#8212; Eli Grey</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maxium-scale=1" />
+<!--[if lt IE 9]><script type="text/javascript" src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<link rel="alternate" type="application/atom+xml" title="Eli Grey" href="http://feeds.feedburner.com/eligrey" />
+<link rel="pingback" href="https://eligrey.com/blog/xmlrpc.php" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"https:\/\/eligrey.com\/blog\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.4.2"}};
+			!function(a,b,c){function d(a){var c,d=b.createElement("canvas"),e=d.getContext&&d.getContext("2d"),f=String.fromCharCode;return e&&e.fillText?(e.textBaseline="top",e.font="600 32px Arial","flag"===a?(e.fillText(f(55356,56806,55356,56826),0,0),d.toDataURL().length>3e3):"diversity"===a?(e.fillText(f(55356,57221),0,0),c=e.getImageData(16,16,1,1).data.toString(),e.fillText(f(55356,57221,55356,57343),0,0),c!==e.getImageData(16,16,1,1).data.toString()):("simple"===a?e.fillText(f(55357,56835),0,0):e.fillText(f(55356,57135),0,0),0!==e.getImageData(16,16,1,1).data[0])):!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g;c.supports={simple:d("simple"),flag:d("flag"),unicode8:d("unicode8"),diversity:d("diversity")},c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.simple&&c.supports.flag&&c.supports.unicode8&&c.supports.diversity||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='wp-syntax-css-css'  href='https://eligrey.com/blog/wp-content/plugins/wp-syntax/css/wp-syntax.css?ver=1.0' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack_css-css'  href='https://eligrey.com/blog/wp-content/plugins/jetpack/css/jetpack.css?ver=3.9.1' type='text/css' media='all' />
+<script type='text/javascript' src='https://eligrey.com/blog/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
+<script type='text/javascript' src='https://eligrey.com/blog/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='https://eligrey.com/blog/wp-content/plugins/google-analyticator/external-tracking.min.js?ver=6.4.9'></script>
+<link rel='https://api.w.org/' href='https://eligrey.com/blog/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://eligrey.com/blog/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://eligrey.com/blog/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.4.2" />
+<style type='text/css'>img#wpstats{display:none}</style><!-- Google Analytics Tracking by Google Analyticator 6.4.9: http://www.videousermanuals.com/google-analyticator/ -->
+<script type="text/javascript">
+    var analyticsFileTypes = [''];
+    var analyticsSnippet = 'disabled';
+    var analyticsEventTracking = 'enabled';
+</script>
+<script type="text/javascript">
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-4364432-1', 'auto');
+ 
+	ga('send', 'pageview');
+</script>
+<link rel="stylesheet" href="https://eligrey.com/blog/wp-content/themes/eligrey.com/style.css" type="text/css" />
+</head>
+<body class="error404">
+<div id="content">
+	<header>
+		<h1 class="four04"><a href="https://eligrey.com/blog" title="Eli Grey">Eli Grey</a></h1>
+				
+			</header>
+	
+	<div id="main">
+							<h2 class="four04">Page not found.</h2>
+						<div><form method="get" class="searchform" action="https://eligrey.com/blog">
+					<label for="local-search">Search for it here:</label><input type="search" id="local-search" name="s" value="Blob.js" />
+					<input type="submit" value="&#x1F50D;" title="Search" />
+			</form></div>
+						<div><form method="get" class="searchform" action="https://www.google.com/search">
+				<label for="google-search">Search for it on <span class="google"><span>G</span><span>o</span><span>o</span><span>g</span><span>l</span><span>e</span></span>:</label>
+				<input type="search" id="google-search" name="q" value="Blob.js site:eligrey.com" />
+				<input type="submit" value="&#128269;" title="Google Search" />
+			</form></div>
+				</div>
+	
+	<footer>
+		<a rel="license" href="/about#license">© 2016 Eli Grey</a>
+		<div class="heart">&#10087;</div>
+	</footer>
+</div>
+<script type="application/ecmascript">
+//<![CDATA[
+(function () {
+if (self.chrome && /^Win\d*$/.test(navigator.platform)) {
+	// Fix font smoothing for large text on Chrome on Windows
+	var style = document.createElement("style");
+	document.documentElement.appendChild(style);
+	style.sheet.insertRule(
+		  "h1,h2,h3,h4,h5,h6,.heart,.searchform input[type='submit'],.searchform input[type='search'],.searchform input[type='text']{text-shadow:rgba(0,0,0,.01) 0 0 1px}"
+		, style.sheet.cssRules.length
+	);
+}
+if (/ AppleWebKit/.test(navigator.userAgent) && document.querySelectorAll) {
+	// Fix input type=search styling bug in WebKit by changing type to text
+	var
+		  search_fields = document.querySelectorAll(".searchform input[type='search']")
+		, i = search_fields.length
+	;
+	while (i--) {
+		search_fields[i].type = "text";
+	}
+}
+}());
+//]]>
+</script>
+	<div style="display:none">
+	</div>
+<script type='text/javascript' src='https://s0.wp.com/wp-content/js/devicepx-jetpack.js?ver=201608'></script>
+<script type='text/javascript' src='https://secure.gravatar.com/js/gprofiles.js?ver=2016Febaa'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var WPGroHo = {"my_hash":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://eligrey.com/blog/wp-content/plugins/jetpack/modules/wpgroho.js?ver=4.4.2'></script>
+<script type='text/javascript' src='https://eligrey.com/blog/wp-includes/js/wp-embed.min.js?ver=4.4.2'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var countVars = {"disqusShortname":"eligrey"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://eligrey.com/blog/wp-content/plugins/disqus-comment-system/media/js/count.js?ver=4.4.2'></script>
+<script type='text/javascript' src='https://stats.wp.com/e-201608.js' async defer></script>
+<script type='text/javascript'>
+	_stq = window._stq || [];
+	_stq.push([ 'view', {v:'ext',j:'1:3.9.1',blog:'3674531',post:'0',tz:'-5',srv:'eligrey.com'} ]);
+	_stq.push([ 'clickTrackerInit', '3674531', '0' ]);
+</script>
+</body>
+</html>
---
+++ maps/js/profile.js
@@ -0,0 +1,83 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 500 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var area = d3.svg.area()
+    .x(function(d) { return x(d.dist); })
+    .y0(height)
+    .y1(function(d) { return y(d.alts.DTM25); });
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.
+    style("font-family", "Georgia").
+    style("font-size", "15px").
+    style("font-style", "normal").
+    style("font-variant", "normal").
+    style("font-weight", "normal").
+    style("text-rendering", "optimizeLegibility").
+    style("shape-rendering", "default");
+
+d3.json('profile.json', function(data) {
+  x.domain(d3.extent(data, function(d) { return d.dist; }));
+  y.domain([0, d3.max(data, function(d) { return d.alts.DTM25; })]);
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .style('fill', 'steelblue')
+      .attr("d", area);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Elevation");
+});
+
+d3.select("#generate")
+    .on("click", writeDownloadLink);
+
+function writeDownloadLink(){
+    try {
+        var isFileSaverSupported = !!new Blob();
+    } catch (e) {
+        alert("blob not supported");
+    }
+
+    var html = d3.select("svg")
+        .attr("title", "test2")
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .node().parentNode.innerHTML;
+
+    var blob = new Blob([html], {type: "image/svg+xml"});
+    saveAs(blob, "myProfile.svg");
+};
